6410e0eb050c20552e90228ebe19b15f718f5c49
NEVA <neva.vka@yandex.ru>
2022-10-28 22:19:58 +0300

    Задание 47

--- Dz01/Program.cs
+++ Dz01/Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿Console.Clear();
+
+Console.WriteLine("Введите количество строк :");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов :");
+int n = Convert.ToInt32(Console.ReadLine());
+
+double [,] array = new double [m ,n];
+for (int i = 0; i < array.GetLength(0); i++ )
+{
+    for (int j = 0; j < array.GetLength(1); j++ )
+    {
+         array[i,j] = new Random().Next(-10,10);
+         array[i,j] =Math.Round(array[i,j]/3.0,1);
+        Console.Write($"{array[i,j]} ");
+    }
+    Console.WriteLine();
+}
